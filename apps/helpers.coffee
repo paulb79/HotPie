750118b3a5723bff99629ebb7b8a797afeb27dc7
paulb79 <paulb@line.couk>
2012-08-31 12:00:13 +0100

    Updated this demo with an example of flash messages
    Now uses Redis and connect-flash and sends messages through the session

---
+++ apps/helpers.coffee
@@ -0,0 +1,5 @@
+
+helpers = (app) ->
+
+  
+module.exports = helpers
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ require('coffee-script');
 var express = require('express')
   , RedisStore = require('connect-redis')(express)
   , http = require('http')
+  , url = require('url')
   , path = require('path')
+  , flash = require('connect-flash')
   , stylus = require('stylus'); 
 
 var app = module.exports = express();
@@ -21,13 +23,14 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser());
+  app.use(express.cookieParser('keyboard cat'));
   app.use(express.session({
     secret: "sdfiernf98h34bru8bdbfufbdfhuewf87g",
     store: new RedisStore
-  }))
-  app.use(app.router);
+  }));
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(flash());  
+  app.use(app.router);
 });
 
 app.configure('development', function(){
@@ -43,9 +46,16 @@ app.configure('production', function() {
   app.use(express.errorHandler());
 });
 
-// Routes
+// Global helpers
+require('./apps/helpers')(app);
 
-require('./apps/authentication/routes') (app)
+// Routes
+require('./apps/authentication/routes')(app);
+
+app.get('/', function(req, res) {
+  req.flash('info', 'Hi There');
+  res.render('index', { message: req.flash('info') } );
+});
 
 app.listen(app.settings.port);
 console.log("Express server listening on port %d in %s mode", app.settings.port, app.settings.env);
